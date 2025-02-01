30e94d9bea1a1617214bbb7f7bc0ceeaf69791fe
Clement-Cauet <70692830+Clement-Cauet@users.noreply.github.com>
2025-02-01 15:43:49 +0100

    refactor: update career layout to display PDF resume and remove unused PDF generation workflow

--- assets/js/career.js
+++ assets/js/career.js
@@ -38,27 +38,9 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 document.getElementById('displayCV').addEventListener('click', async () => {
-    const ownerName = document.querySelector('meta[name="owner-name"]').content;
-    try {
-        const response = await fetch(`https://registry.jsonresume.org/${ownerName}`);
-        if (!response.ok) throw new Error('Erreur lors du chargement du contenu');
-        const htmlContent = await response.text();
-        openModalWithContent(htmlContent);
-    } catch (error) {
-        console.error("Erreur lors du chargement du CV :", error);
-    }
-});
-
-function openModalWithContent(content) {
-    const iframe = document.getElementById('modal-iframe');
-    const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
-    iframeDoc.open();
-    iframeDoc.write(content);
-    iframeDoc.close();
-
     document.querySelector('.modal').style.display = 'flex';
     document.querySelector('.overlay').style.display = 'flex';
-}
+});
 
 document.querySelector('.overlay').addEventListener('click', closeModal);
 document.querySelector('.close').addEventListener('click', closeModal);
@@ -67,24 +49,3 @@ function closeModal() {
     document.querySelector('.modal').style.display = 'none';
     document.querySelector('.overlay').style.display = 'none';
 }
-
-document.querySelector('.download').addEventListener('click', async () => {
-    const ownerName = document.querySelector('meta[name="owner-name"]').content;
-    const response = await fetch(`https://registry.jsonresume.org/${ownerName}`);
-    const data = await response.text();
-
-    const iframe = document.getElementById('modal-iframe');
-    const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
-    iframeDoc.body.innerHTML = data;
-
-    const date = new Date().toISOString().split('T')[0];
-    const fileName = `${resume.basics.name.replace(/\s+/g, '-').toLowerCase()}-resume-${date}.pdf`;
-    const opt = {
-        margin: 0,
-        filename: fileName,
-        html2canvas: { scale: 1, useCORS: true },
-        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
-    };
-
-    html2pdf().set(opt).from(iframeDoc.body).save();
-});
